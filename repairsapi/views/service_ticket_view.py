3c1f3c9977144bd53533fc3abc11aca540ca4dde
Ricky Canepa <rickycanepa.rc@gmail.com>
2023-02-15 01:01:26 -0600

    added ticket filter

--- repairsapi/views/service_ticket_view.py
+++ repairsapi/views/service_ticket_view.py
@@ -3,27 +3,48 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from repairsapi.models import ServiceTicket, Employee
+from repairsapi.models import ServiceTicket, Employee, Customer
 
 
 class ServiceTicketView(ViewSet):
     """Honey Rae API service tickets view"""
 
-    def list(self, request):
-        """Handle GET requests to get all service tickets
+    def create(self, request):
+        """Handle POST requests for service tickets
 
         Returns:
-            Response -- JSON serialized list of service tickets
+            Response: JSON serialized representation of newly created service ticket
         """
+        new_ticket = ServiceTicket()
+        new_ticket.customer = Customer.objects.get(user=request.auth.user)
+        new_ticket.description = request.data['description']
+        new_ticket.emergency = request.data['emergency']
+        new_ticket.save()
+
+        serialized = ServiceTicketSerializer(new_ticket, many=False)
+
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+
+    def list(self, request):
+        service_tickets = []
+
 
         if request.auth.user.is_staff:
             service_tickets = ServiceTicket.objects.all()
+
+            if "status" in request.query_params:
+                if request.query_params['status'] == "done":
+                    service_tickets = service_tickets.filter(date_completed__isnull=False)
+
+                if request.query_params['status'] == "all":
+                    pass
+
         else:
             service_tickets = ServiceTicket.objects.filter(customer__user=request.auth.user)
 
         serialized = ServiceTicketSerializer(service_tickets, many=True)
         return Response(serialized.data, status=status.HTTP_200_OK)
-
+        
     def retrieve(self, request, pk=None):
         """Handle GET requests for single service ticket
 
@@ -41,9 +62,17 @@ class TicketEmployeeSerializer(serializers.ModelSerializer):
         model = Employee
         fields = ('id', 'specialty', 'full_name')
 
+class TicketCustomerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Customer
+        fields = ('id', 'address', 'full_name')
+
 class ServiceTicketSerializer(serializers.ModelSerializer):
     """JSON serializer for service tickets"""
     employee = TicketEmployeeSerializer(many=False)
+    customer = TicketCustomerSerializer(many=False)
+
 
     class Meta:
         model = ServiceTicket
